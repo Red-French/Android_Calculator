2edae92906bcdb8c4908607f7b994ff6fbd0f829
Red-French <red@redfrench.net>
2016-08-16 21:00:56 -0500

    initial commit - change theme path

---
+++ app/src/main/java/net/redfrench/marshmallow_calculator/CalcActivity.java
@@ -0,0 +1,13 @@
+package net.redfrench.marshmallow_calculator;
+
+import android.app.Activity;  // theme path
+import android.os.Bundle;
+
+public class CalcActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
